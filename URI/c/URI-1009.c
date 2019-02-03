8630d9ff4a3ae57c8cb23086956e4fd9582ef365
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-02-03 00:07:53 -0600

    URI-1009

--- URI/c/URI-1009.c
+++ URI/c/URI-1009.c
@@ -1,11 +1,26 @@
-#include<stdio.h>
-int main()
-{
-char name;
-double salary, value, TOTAL;
-scanf("%s %lf %lf", &name, &salary, &value);
+#include <stdio.h>
 
-TOTAL = salary+value*.15;
-printf("TOTAL = R$ %.2lf\n", TOTAL);
-return 0;
-}
+/* Escriba un programa que lea el nombre de un vendedor,
+* su salario fijo y el total de las ventas realizadas por
+* él en el mes (en dinero). Considerando que el vendedor recibe
+* un 15% de los productos vendidos, escribir el salario final (total)
+* de cada vendedor a fin de mes, con dos cifras decimales.
+*
+* - No olvide de imprimir un final de linea luego del resultado,
+* de otra forma recibirá “Presentation Error”.
+* - No olvide los espacios en blanco.
+*/
+
+int main(){
+	char name[20];
+	double SALARY, VENTAS, TOTAL;
+
+	scanf("%s", name);
+	scanf("%lf", &SALARY);
+	scanf("%lf", &VENTAS);
+
+	TOTAL = SALARY+(VENTAS*15)/100;
+
+	printf("TOTAL = R$ %.2f\n", TOTAL);
+	return 0;
+ }
