703abf51375206252cfe5027e4dfedc37ca342f2
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-02-03 21:50:15 -0600

    URI-1036

---
+++ URI/c/URI-1036.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Leer 3 números de punto flotante.
+* Luego, imprimir las raíces de la fórmula de Bhaskara.
+* Si es imposible calcular las raíces debido a
+* una división por cero ó a la raíz
+* cuadrada de un número negativo, presentar el
+* mensaje “Impossivel calcular”.
+*/
+
+int main(){
+	float A,B,C;
+
+	scanf("%f %f %f", &A,&B,&C);
+
+	
+}
