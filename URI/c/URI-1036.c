6155ef74ea55ff7e2373dbb2c3e0f5ce7e5b6b95
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-02-03 23:10:57 -0600

    URI-1036

--- URI/c/URI-1036.c
+++ URI/c/URI-1036.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 /* Leer 3 números de punto flotante.
 * Luego, imprimir las raíces de la fórmula de Bhaskara.
+* FORMULA GENERAL pa'la banda: CHICHARRONERA
 * Si es imposible calcular las raíces debido a
 * una división por cero ó a la raíz
 * cuadrada de un número negativo, presentar el
@@ -9,9 +11,20 @@
 */
 
 int main(){
-	float A,B,C;
+	float A,B,C, DELTA, X1, X2;
 
 	scanf("%f %f %f", &A,&B,&C);
 
-	
+	DELTA = B*B-(4*A*C);
+
+	if (2*A != 0 && DELTA >= 0){
+			X1 = (-B + sqrt(DELTA)) / (2*A);
+			X2 = (-B - sqrt(DELTA)) / (2*A);
+
+			printf("R1 = %.5f\n", X1);
+			printf("R2 = %.5f\n", X2);
+			return 0;
+		}
+	printf("Impossivel calcular\n");
+	return 0;
 }
