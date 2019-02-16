52765f774f501ba513f16942132b75eff5868a71
CesarTavo <cesartavo@ciencias.unam.mx>
2019-02-15 23:47:51 -0600

    Incremento Salarial

---
+++ URI/c/URI-1048.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* La empresa ABC decidió dar un aumento salarial a sus empleados, según la siguiente tabla:
+* Salario		Porcentaje de Reajuste
+* 0-400.00			15%
+* 400.01-800.00			12%
+* 800.01-1200.00		10%
+* 1200.01-2000.00		7%
+* Por encima de 2000.00		4%
+*/
+
+int main(){
+return 0;
+}
