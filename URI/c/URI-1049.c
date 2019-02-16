1253ba0b47a5aafade83e12fd37fa6215586d097
CesarTavo <cesartavo@ciencias.unam.mx>
2019-02-16 02:15:20 -0600

    not finish

---
+++ URI/c/URI-1049.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* En este problema, su trabajo es leer tres palabras en portugués. 
+ * Estas palabras definen un animal de acuerdo con la siguiente tabla, 
+ * de izquierda a derecha. 
+ * Después, imprima el animal elegido definido por estas tres palabras.
+ * https://www.urionlinejudge.com.br/judge/es/problems/view/1049
+*/
+
+int main(){
+
+	char class1[], class2[], class3[], class4[];
+
+	scanf("%s %s %s", class1[], class2[], class3[]);
+
+	class4[] = "hola";
+}
+
+
