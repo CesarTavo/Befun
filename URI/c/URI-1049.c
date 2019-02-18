e819212d99359c7a409eb770cef969fa13d2dbda
CesarTavo <cesartavo@ciencias.unam.mx>
2019-02-18 11:34:31 -0600

    not finish

--- URI/c/URI-1049.c
+++ URI/c/URI-1049.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* En este problema, su trabajo es leer tres palabras en portugués. 
  * Estas palabras definen un animal de acuerdo con la siguiente tabla, 
@@ -9,11 +10,60 @@
 
 int main(){
 
-	char class1[], class2[], class3[], class4[];
+	char str1[22], str2[22], str3[22],
+		str11[] = "vertebrado",
+		str12[] = "invertebrado",
+		str21[] = "ave",
+		str22[] = "mamifero",
+		str23[] = "inseto",
+		str24[] = "anelideo",
+		str31[] = "carnivoro",
+		str32[] = "onivoro",
+		str33[] = "onivoro",
+		str34[] = "herviboro",
+		str35[] = "hematofago",
+		str36[] = "herviboro",
+		str37[] = "hematofago",
+		str38[] = "onivoro";
 
-	scanf("%s %s %s", class1[], class2[], class3[]);
+	scanf("%s", &str1);
+	scanf("%s", &str2);
+	scanf("%s", &str3);
 
-	class4[] = "hola";
+	if(strcmp(str1, str11) == 0){
+		if(strcmp(str2, str21) == 0){
+			if(strcmp(str3, str31) == 0){
+				printf("aguia\n");
+				return 0;	
+			} 
+			printf("pomba\n");
+			return 0;			
+	} else if(strcmp(str2, str22) == 0){
+		if(strcmp(str3, str33) == 0){
+			printf("homem\n");
+			return 0;
+		} printf("vaca\n");
+		return 0;
+	}
+	} else if(strcmp(str1, str12) == 0){
+		if(strcmp(str2, str23) == 0){
+			if(strcmp(str3, str35) == 0){
+				printf("pulga\n");
+				return 0;
+			} 
+			printf("lagarta\n");
+			return 0;
+		}else if(strcmp(str2, str24) == 0){
+			if(strcmp(str3, str37) == 0){
+				printf("sanguessuga\n");
+				return 0;
+			}
+			printf("minhoca\n");
+			return 0;
+		}
+		return 0;
+	}
+	return 0;
 }
 
 
