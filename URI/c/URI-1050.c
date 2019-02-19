b190497fbc77699b2dd63a03cb5373146578649f
CesarTavo <cesartavo@ciencias.unam.mx>
2019-02-19 01:48:59 -0600

    another game

---
+++ URI/c/URI-1050.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/* Lea un número entero que corresponda al número de código de área para marcación telefónica.
+ *  A continuación, imprima el destino de acuerdo con la siguiente tabla:
+ * DDD	| Destination
+ * 61 	| Salvador
+ * 71	| Sao Paulo
+ * 21	| Rio de Janeiro
+ * 32	| Juiz de Fora
+ * 19	| Campinas
+ * 27	| Vitoria
+ * 31	| Belo Horizonte
+ * Si el número de entrada no se encuentra en la tabla anterior,
+ * la salida debe ser:
+ * DDD nao cadastrado
+ * Que significa “DDD no encontrado” in Portugues.
+ */
+
+int main(){
+	return 0;	
+}
