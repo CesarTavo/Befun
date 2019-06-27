b34e7de13448111d2360fffa0e1840607321a7fe
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-06-26 23:42:16 -0500

    Fixed Password

---
+++ URI/c/URI-1114.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Write a program that keep reading a password until it is valid. 
+ * For each wrong password read, write the message "Senha inválida". 
+ * When the password is typed correctly print the message "Acesso Permitido" 
+ * and finished the program. The correct password is the number 2002.
+ *
+ * Input
+ * The input file contains several tests cases. 
+ * Each test case contains only an integer number.
+ *
+ * Output
+ * For each number read print a message 
+ * corresponding to the description of the problem.
+ */
+
+int main(){
+	int pass;
+	while(1){
+		scanf("%d", &pass);
+		if(pass==2002){
+			printf("Acesso Permitido\n");
+			return 0;
+		}else{
+			printf("Senha Invalida\n");
+		}
+	}
+	return 0;
+}
