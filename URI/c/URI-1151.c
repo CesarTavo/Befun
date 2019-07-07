515496fc3ace00ecd8663533ea327a739da5f4d6
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-07-06 19:33:09 -0500

    Easy Fibonacci

---
+++ URI/c/URI-1151.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int num, i, x, y;
+
+	scanf("%d", &num);
+	if(0<num && num<46){
+		for(i=0;i<num-1;i++){
+			if(i==0){
+				printf("0 ");
+				x=1;
+			}else if(i==1){
+				printf("1 ");
+				y=1;
+			}else{
+				x=x+y;
+				y=x-y;
+				printf("%d ", y);
+			}
+		}
+		printf("%d\n", x);
+	}
+	return 0;
+}
