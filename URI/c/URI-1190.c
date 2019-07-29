cd78b123bd5853499a7b13af3dd7c4b1941e921f
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-07-28 23:39:40 -0500

    Right Area

---
+++ URI/c/URI-1190.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/* Read an uppercase character that indicates an operation that 
+/ will be performed in an array M[12][12]. 
+/ Then, calculate and print the sum or average 
+/ considering only that numbers 
+/ that are below of the triangle 
+*/
+
+int main(){
+  double arr[12][12], sum=0, av=0;
+  int i, j, a, b;
+  char c;
+
+  scanf(" %c", &c);
+
+  for(i=0;i<12;i++){
+    for(j=0;j<12;j++){
+      scanf("%lf", &arr[i][j]);
+    }
+  }
+
+  for(i=0,a=12;i<6;i++,a--){
+    for(j=a;j<12;j++){
+      sum += arr[i][j];
+      av++;
+      //printf("%.1lf ", arr[i][j]);
+    }
+    //printf("\n");
+  }
+
+  for(i=6,a=7;i<12;i++,a++){
+    for(j=a;j<12;j++){
+      sum += arr[i][j];
+      av++;
+      //printf("%.1lf ", arr[i][j]);
+    }
+    //printf("\n");
+  }
+  
+
+  if(c=='S')
+    printf("%.1lf\n", sum);
+  else if(c=='M')
+    printf("%.1lf\n", sum/av);
+  
+  return 0;
+}
