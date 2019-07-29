1ca80b4ff84d75dfefed43a5b874cc8fb397f3b3
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-07-28 22:06:12 -0500

    Above the Secundary Diagonal

---
+++ URI/c/URI-1185.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* Read an uppercase character that indicates an operation that 
+/ will be performed in an array M[12][12]. 
+/ Then, calculate and print the sum or average 
+/ considering only that numbers that are below the main diagonal of the array
+*/
+
+int main(){
+  double arr[12][12];
+  int i, j, a;
+  float sum=0, av=0;
+  char c;
+
+  scanf(" %c", &c);
+
+  for(i=0;i<12;i++){
+    for(j=0;j<12;j++){
+      scanf("%lf", &arr[i][j]);
+    }
+  }
+
+  for(i=0,a=11;i<12;i++,a--){
+    for(j=0;j<a;j++){
+      sum += arr[i][j];
+      av++;
+    }
+  }
+
+  if(c=='S')
+    printf("%.1lf\n", sum);
+  else if(c=='M')
+    printf("%.1lf\n", sum/av);
+  
+  return 0;
+}
