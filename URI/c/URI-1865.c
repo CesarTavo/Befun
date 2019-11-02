471fe7e17ea23ed9931b0a511a754a224cbcbe9f
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2019-11-02 15:57:53 -0600

    Mjölnir

--- URI/c/URI-1865.c
+++ URI/c/URI-1865.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+	char name[12];
+	int n, i, c;
+
+	scanf("%d", &c);
+	for(i=1;i<=c;i++){
+		scanf("%s %d", name, &n);
+
+		if(strcmp(name, "Thor") == 0){
+			printf("Y\n");
+		}else{
+			printf("N\n");
+		}
+	}
+
+	return 0;
 }
